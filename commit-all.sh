c469a4def5d49ab1a6a81b8583b0432179feb52b
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2023-10-07 00:04:49 -0500

    update

--- commit-all.sh
+++ commit-all.sh
@@ -1,22 +1,76 @@
 #! /usr/bin/env bash
-set -euxo nounset
-(( $UID ))
-if (( ! $# )) ; then
-  M=update
-else M="$*"
+set -euxo nounset -o pipefail
+
+if (( ! UID )) ; then
+echo de-escalate privileges 1>&2
+exit 1
 fi
 
-find -mindepth 1 -maxdepth 5 -type d |
-while IFS= read -r k ; do
-[[ -d "$k.git" ]] || continue
-echo "$k"
-git -C "$k" add .
-git -C "$k" commit -m "$M"
-git -C "$k" push
-done
+if (( ! $# )) ; then
+  M=update
+else M="$@"
+fi
 
-git pull
-git add .
-git commit -m update
+#find -mindepth 1 -maxdepth 5 -type d |
+#while IFS= read -r k ; do
+#[[ -d "$k.git" ]] || continue
+#echo "$k"
+#git -C "$k" add .
+#git -C "$k" commit -m "$M"
+#git -C "$k" push
+#done
+
+#git pull
+#git add .
+#git commit -m update
+#git push
+
+#contrib/afl/AFLPlusPlus
+#contrib/xmrig/xmrig
+while IFS= read -r line ; do (
+  echo line: $line
+  IFS=\  read -ra cols <<< "$line"
+  case ${#cols[@]} in
+  1) cols=( "${cols[@]}" main ) ;;
+  2) ;;
+  *) echo unexpected number of columns: $line 1>&2
+     exit 1
+     ;;
+  esac
+  echo cols: ${cols[@]}
+
+  subm="${cols[0]}"
+  brch="${cols[1]}"
+
+  git -C "$subm" pull origin "$brch"
+  git -C "$subm" add .
+  git -C "$subm" commit -m "$M" || :
+  git -C "$subm" push --set-upstream origin "HEAD:$brch"
+) ; done << "EOF"
+dev/bash-syslog
+dev/dcc/kali      kali
+dev/dcc/main
+dev/dcc/ubuntu    ubuntu
+dev/dcc/voidlinux voidlinux
+htb/db
+htb/dns
+htb/import_db
+htb/msfrpcd
+htb/nmap
+htb/recond
+htb/rest
+htw/aircrack/main
+htw/aircrack/pg   pg
+htw/aircrack/pgo  pgo
+htw/capsrv
+it/ca
+it/pgldap
+it/pgrsyslog
+it/web
+ops/ppa
+EOF
+
+git add -A
+git commit -m "$M"
 git push
 
